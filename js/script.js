ac7c7b6a671dbb2b51b530508f73c7299b5994b2
Daniel Healy <daniel.addison.healy@gmail.com>
2020-10-12 18:04:11 -0400

    Hides Colors Based on Theme

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function showHideOtherTitle(){
     if (titleSelect.value === "other"){
         otherLabel.style.display = "block"; // shows label
         otherInput.style.display = "block"; // shows input
-    }else{
+    } else {
         otherInput.value = ""; // clears any leftover data that may have been input before the hide was triggered
 
         otherLabel.style.display = "none"; // hides label
@@ -18,4 +18,48 @@ function showHideOtherTitle(){
 
 }
 
+function processThemeChange() {
+    const designSelect = document.getElementById("design");
+    const colors = document.getElementById("color").children;
+    const jsPunsColors = ["cornflowerblue", "darkslategrey", "gold"];
+    const heartJSColors = ["tomato", "steelblue", "dimgrey"];
+
+    if (designSelect.value === "select-theme") {
+        showColors(colors, colors);
+
+        document.getElementById("color-error").style = "block";
+
+    } else {
+        if (designSelect.value === "js puns") {
+            showColors(colors, jsPunsColors);
+        }else if (designSelect.value === "heart js") {
+            showColors(colors, heartJSColors);
+        }
+    }
+}
+
+function showColors(colors, themeColors){
+    
+    outerloop:
+    for (let i = 0; i < colors.length; i++) {
+        let matchFound = false;
+
+        innerloop:
+        for (let j = 0; j < themeColors.length; j++) {
+            if (colors[i].value === themeColors[j]) {
+                colors[i].style.display = "block";
+                matchFound = true;
+                break innerloop;
+            }
+        }
+
+        if (matchFound === false) {
+            colors[i].style.display = "none";
+        }
+
+        
+    }
+}
+
 showHideOtherTitle(); // hide the other job role field on page load
+processThemeChange(); // hide colors on 
